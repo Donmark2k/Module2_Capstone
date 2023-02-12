7987ecd5585525c31cd483a496b9e144f54b601b
Donmark2k <chukwuemekavp@gmail.com>
2023-02-12 15:10:53 +0100

    Implement the close button in the popup modal

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import { movieUrl, url } from './modules/api.js';
 import layout from './modules/homePageLayout';
 import movieCount from './modules/movieCount.js';
 import {fetchLike, newLike} from './modules/fetchlike.js';
-
+import { commentForm, reserveForm} from './modules/popForm.js';
+import {resLayout, listLayOut} from './modules/resCommentLayout.js';
+import  {fetchCommentApi, fetchResApi} from './modules/formApi.js';
+import addComment from './modules/addResComment.js';
+import popUp from './modules/popUp.js';
 
 const movies = async () => {
     const res = await fetch ( movieUrl );
@@ -21,6 +25,48 @@ movies().then((movie) => {
         const card = document.querySelectorAll('.list');
          fetchLike(card)
          newLike ( heart, index, each.show.id);
+
+        //  Calling comment Event listener from the comment button
+        const comment = document.querySelectorAll('#comment')
+        comment.forEach((com, ind) => {
+            com.addEventListener('click', () => {
+                if (index === ind) {
+                    const lists = [];
+                    popUp(each, 'comment');
+                    commentForm();
+                    // fetchCommentApi(each.show.id, lists)
+
+                // Collect form inputs
+                const form = document.querySelector('form');
+                const user = document.querySelector('#name');
+                const text = document.querySelector('#text');
+                const comsec = document.querySelector('.test');
+                form.addEventListener('submit', (e)=>{
+                    e.preventDefault();
+                    if (user.value ===''|| text.value === '') {
+                        return
+                    }
+                    addComment(each.show.id, user.value, text.value)
+                    lists.push ({ creation_date: 'few minutes ago', username: user.value, comment: text.value});
+                    comsec.innerHTML ='';
+                    lists.map((e)=> listLayOut(e, comsec));
+                    form.reset();
+                    const header = document.querySelector('.head');
+                    const test = document.querySelector('.test')
+                     movieCount(test, header)
+                });
+
+                // Cancel the pop up icon
+                const X = document.querySelector('.X');
+                X.addEventListener('click', ()=> {
+                    const pop = document.querySelector('.pop')
+                    pop.remove();
+                    pop.innerHTML = '';
+                })
+
+                }
+            })
+        })
 
         return each;
     })
---
+++ src/modules/resCommentLayout.js
@@ -0,0 +1,15 @@
+const listLayOut = (e, comsec) => {
+    const temp = `${e.creation_date} ${e.username} : ${e.comment}`;
+    const per = document.createElement ('li')
+    per.innerHTML = temp;
+    comsec.appendChild(per)
+}
+
+const resLayout = (e, comsec) => {
+    const temp = `${e.date_start} -${e.date_end}  by ${e.username}`;
+    const per = document.createElement('li');
+    per.innerHTML = temp;
+    comsec.appendChild(per);
+  };
+  
+  export  {resLayout, listLayOut};
